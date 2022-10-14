de12ab2dd6868f36b0a61c91301825d3da0029b6
Vilio <kontakt@wiliam.sk>
2022-10-14 13:56:31 +0200

    JS validacia formularu a zobrazenie chybnej spravy

--- features.js
+++ features.js
@@ -10,6 +10,34 @@ function js_change_body_color()
   document.getElementById("js_output").innerHTML = "Farba bola zmenena na " + custom_color;
 }
 
+function js_form_validate()
+{
+    const form_html = document.forms['formular'];
+    let result = true;
+
+    for(i=0 ; i < form_html.length; i++)
+    {
+      if(form_html[i].name == "submit")
+        continue;
+      if(form_html[i].value == "")
+      {
+        // nevyplnena polozka formularu
+        result = false;
+        form_html[i].style.border = "2px solid red";
+
+        // pridaj poznamku k policku
+        const html_span = document.createElement('span');
+        const html_text = document.createTextNode('nevyplnena polozka');
+        html_span.appendChild(html_text);
+
+        html_final = form_html[i].parentNode.appendChild(html_span);
+        html_final.style.color = "red";
+      }
+    }
+
+    return result;
+}
+
 function js_show_form_items()
 {
     const form_html = document.forms['formular'];
